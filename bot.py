61c409800bba1fabdb53c086a8a515654f0726d2
TimurKHidk <96879368+TimurKHidk@users.noreply.github.com>
2022-04-15 01:05:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,19 @@
 import discord
-import asyncpraw
-import asyncio
+import aiohttp
+from discord.ext import commands
+import random
 
 
 TOKEN ='OTYzODExMTU0NzE3NzI4Nzc4.YlbhFg.Dd7I1Df0L4Qggqv56EJsGwcpzAQ'
-bot = discord.Client()
-reddit = asyncpraw.Reddit(client_id='sT4hZMkd5hT3srsoB_brQg' , 
-                          client_secret='SCOzIjdxIvbmev7Y68lVJGAw8QMg8w',
-                          user_agent='random_raddit_bot/0.0.1')
+bot = commands.Bot(command_prefix=('+'))
 
-mems = []
-timeout = 5
-channel_id = 963510501353062453
-reddit = 'memes'
-plimit = 1
-
-
-@bot.event
-async def on_ready():
-    channel = bot.get_channel(channel_id)
-    while True:
-        await asyncio.sleep(timeout)
-        memes_submissions = await reddit.subreddit(reddit)
-        memes_submissions = memes_submissions.hot(limit=plimit)
-        item = await memes_submissions.__anext__()
-        if item.title not in mems:
-            mems.append(item.title)
-            await channel.send(item.url)
+@bot.command()    
+async def mem(ctx):
+        embed = discord.Embed(title="Memes", description="memes from reddit")  
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get('https://www.reddit.com/r/memes/new.json?sort=hot') as r:
+                res = await r.json()
+                embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
+                await ctx.send(embed=embed)
 
 bot.run(TOKEN)
